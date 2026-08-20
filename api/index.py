356db6d4344310328ec40377b40d6a43eeb8f076
javadkaramzadeh6-art <javadkaramzadeh6@gmail.com>
2026-08-20 18:35:45 +0330

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,27 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
         self.end_headers()
         self.wfile.write(b"Rahavard Bot is online!")
+
+    def do_POST(self):
+        content_length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(content_length)
+
+        try:
+            update = json.loads(body.decode("utf-8"))
+            print("Telegram update:", update)
+
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(b'{"ok":true}')
+
+        except Exception as e:
+            print("ERROR:", e)
+            self.send_response(500)
+            self.end_headers()
